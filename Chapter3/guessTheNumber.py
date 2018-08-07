346209a93505abba8a7792c199051a31e5071ee8
Ki Song <kiwisquash@gmail.com>
2018-08-06 22:00:21 -0700

    Cleaned up the code to reduce instances of input based on the example in the book. I also added special cases for the singular form.

--- Chapter3/guessTheNumber.py
+++ Chapter3/guessTheNumber.py
@@ -6,22 +6,26 @@ nGuessLeft = nGuess
 correctGuess = random.randint(1,20)
 print("I am thinking of a number between 1 and 20.")
 print("You have 6 tries to guess the number correctly.")
-guess = input("What is your guess?\n")
-for i in range(nGuess):
-    nGuessLeft = nGuessLeft - 1  
+for numTry in range(nGuess):
+    guess = input("What is your guess?\n")
     while not inputValidator.isInteger(guess):
         guess = input("Please enter an integer.\n")
+    nGuessLeft = nGuessLeft - 1  
     guess = int(guess)
-    if guess == correctGuess:
-        print("Congrats! You got it!")
-        break
-    elif guess < correctGuess:
+    if guess < correctGuess:
         print("That's a bit low.",end=" ")
-    else:
+    elif guess > correctGuess:
         print("That's a bit high.",end=" ")
-    if nGuessLeft > 1:
-        guess = input("You have "+str( nGuessLeft )+" guesses left!\n")
-    elif nGuessLeft ==1:
-        guess = input("You have "+str( nGuessLeft )+" guess left!\n")
     else:
-        print("\nYou are out of guesses!\nThe correct number was "+str ( correctGuess )+".")
+        break
+    if nGuessLeft > 1:
+        guess = print("You have "+str( nGuessLeft )+" guesses left!\n")
+    elif nGuessLeft ==1:
+        guess = print("You have "+str( nGuessLeft )+" guess left!\n")
+if guess == correctGuess:
+    if numTry > 1:
+        print("Congrats! You got it in "+str(numTry)+ " guesses!")
+    else:
+        print("Amazing! You got it in a single shot! (Are you a psychic?)")
+else:
+    print("\nYou are out of guesses!\nThe correct number was "+str ( correctGuess )+".")
